be8ff45bdc080d79d322b8288ecac97ae0440380
robin00007 <76676218+robin00007@users.noreply.github.com>
2023-05-14 13:42:04 +0530

    cards added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,30 +1,30 @@
 import styles from "../styles/header.module.css";
 import { HiMenu } from "react-icons/hi";
+import logo from "../assets/logo.svg";
 
 const Header = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        <div className={styles.logo}>logo</div>
+        <div className={styles.logo}> <img src={logo} alt="" /></div>
         <div className={styles.links}>
           <div className={styles.routes}>
             <a href="">About</a>
             <a href="">Whitepaper</a>
             <a href="">
-              Transaction 
+              Transaction
               {/* <span>coming soon ...</span>{" "} */}
             </a>
             <a href="">
-              Brdge 
+              Brdge
               {/* <span>coming soon ...</span>{" "} */}
             </a>
           </div>
-         
         </div>
         <div className={styles.menu}>
-            <div className={styles.connectBtn}>Connect wallet</div>
-            <HiMenu size="2em" />
-          </div>
+          <button className={styles.connectBtn}>Connect wallet</button>
+          <HiMenu size="2em" />
+        </div>
       </div>
     </div>
   );
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
             autoplay
             loop
             src="https://assets1.lottiefiles.com/packages/lf20_8lcyef9f.json"
-            style={{ alignItem:'center',width: "95%" }}
+            style={{ alignItem: "center", width: "95%" }}
           >
             <Controls
               visible={false}
@@ -18,10 +18,37 @@ const Home = () => {
           </Player>
         </div>
         <div className={styles.heroDiscription}>
-            <p className={styles.note}>0% trading commission</p>
-            <p className={styles.heading}>The Bridge We Need </p>
-            <p className={styles.subheading}>OrdBridge is a Cross-chain two-way bridge between Bitcoin & Etherium Network </p>
-            <button className={styles.tradingBtn}>Read Docs</button>
+          <p className={styles.note}>0% trading commission</p>
+          <p className={styles.heading}>The Bridge We Need </p>
+          <p className={styles.subheading}>
+            OrdBridge is a Cross-chain two-way bridge between Bitcoin & Etherium
+            Network{" "}
+          </p>
+          <button className={styles.tradingBtn}>Read Docs</button>
+        </div>
+        <div className={styles.cards}>
+          <div className={styles.card}>
+            <p className={styles.topic}>Safe & Reliable</p>
+            <p>
+              OrdBridge is non-custodial & decentralised , processes multi-chain
+              bidge and smart contracts methods on ther chains .
+            </p>{" "}
+          </div>
+          <div className={styles.card}>
+            <p className={styles.topic}>Fast & two way</p>
+            <p>
+              OrdBridge is able to offer extremly fast bridge with zero slippage
+              . Also its tow ways , so assets can be moved from BRC20 to EKC20 &
+              vice versa .
+            </p>
+          </div>
+          <div className={styles.card}>
+            <p className={styles.topic}>Low Fees</p>
+            <p>
+              OrdBridge charges a fees of 1% which is distributed among the
+              token holders of ordBridge{" "}
+            </p>{" "}
+          </div>
         </div>
       </div>
     </div>
